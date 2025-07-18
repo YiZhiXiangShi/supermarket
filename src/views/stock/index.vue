38b034dd867ef1d1224f233f985345daab464c1d
刘威 <3011809287@qq.com>
2025-07-18 15:34:47 +0800

    功能完善

--- src/views/stock/index.vue
+++ src/views/stock/index.vue
@@ -81,15 +81,18 @@ export default {
           request({
             url: `/product/barcode/${item.barcode}`,
             method: 'get'
-          }).then(res => ({
-            found: true,
-            data: res.data,
-            quantity: item.quantity,
-            barcode: item.barcode
-          })).catch(() => ({ found: false, barcode: item.barcode }))
+          }).then(res => {
+            console.log('查商品结果:', res)
+            return {
+              found: !!(res && res.barcodeNo),
+              data: res,
+              quantity: item.quantity,
+              barcode: item.barcode
+            }
+          }).catch(() => ({ found: false, barcode: item.barcode }))
         ))
         // 只保留查到的商品
-        const validRows = results.filter(r => r.found && r.data).map(r => ({
+        const validRows = results.filter(r => r.found).map(r => ({
           serialNo: r.data.serialNo,
           barcode: r.data.barcodeNo,
           productName: r.data.productName,
@@ -201,15 +204,16 @@ export default {
           url: `/product/barcode/${this.barcode}`,
           method: 'get'
         })
-        if (res && res.data) {
-          const existIndex = this.tableData.findIndex(item => item.barcode === res.data.barcodeNo)
+        console.log('接口响应:', res)
+        if (res) {
+          const existIndex = this.tableData.findIndex(item => item.barcode === res.barcodeNo)
           if (existIndex === -1) {
             // 商品不存在，添加新行
             this.tableData.push({
-              serialNo: res.data.serialNo,
-              barcode: res.data.barcodeNo,
-              productName: res.data.productName,
-              unit: res.data.unit,
+              serialNo: res.serialNo,
+              barcode: res.barcodeNo,
+              productName: res.productName,
+              unit: res.unit,
               quantity: 1
             })
             this.$message.success('商品已添加到表格')
